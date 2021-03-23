8ce0310b04c71408a537d6f8b8e135a08173b2c2
anaclaragl <75282985+anaclaragl@users.noreply.github.com>
2021-03-23 16:57:55 -0300

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://git.heroku.com/musicnovum.git"
+  baseURL: "https://musicnovum.herokuapp.com/"
 })
 
 export default api;
